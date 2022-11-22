bad9d12e4d3ef6577f9a7d305dc7907ff43ac30c
Julian Christl <julian.christl@tum.de>
2022-11-22 11:55:59 +0100

    clean up permissions, add todos

--- src/main/java/de/tum/in/www1/artemis/web/rest/LogsResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/LogsResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
+import de.tum.in.www1.artemis.security.annotations.EnforceAdmin;
 import de.tum.in.www1.artemis.web.rest.vm.LoggerVM;
 
 /**
@@ -16,15 +17,21 @@ import de.tum.in.www1.artemis.web.rest.vm.LoggerVM;
 @RestController
 @RequestMapping("/management")
 // is automatically secured and can only be invoked by Admins
+// TODO: are we sure about that?
 public class LogsResource {
 
+    // TODO: /admin
+    // TODO: Is this actually public?
     @GetMapping("/logs")
+    @EnforceAdmin
     public List<LoggerVM> getList() {
         LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
         return context.getLoggerList().stream().map(LoggerVM::new).toList();
     }
 
+    // TODO: /admin
     @PutMapping("/logs")
+    @EnforceAdmin
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void changeLevel(@RequestBody LoggerVM jsonLogger) {
         LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
